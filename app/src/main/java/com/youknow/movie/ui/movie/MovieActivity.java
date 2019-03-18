4bb42180142c1a1144ff842bd21e6ff0ba0d8a8d
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-03-18 20:56:59 +0900

    View 정의

--- app/src/main/java/com/youknow/movie/ui/movie/MovieActivity.java
+++ app/src/main/java/com/youknow/movie/ui/movie/MovieActivity.java
@@ -1,13 +1,15 @@
-package com.youknow.movie;
+package com.youknow.movie.ui.movie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.youknow.movie.R;
+
+public class MovieActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie);
     }
 }
---
+++ app/src/main/java/com/youknow/movie/ui/movies/MoviesActivity.java
@@ -0,0 +1,15 @@
+package com.youknow.movie.ui.movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.youknow.movie.R;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
