a470e64fb920723eb5ef455724e37bec64304df0
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2019-05-21 17:18:47 +0900

    불필요 데이터 제거

--- kotlin-rx/data/src/main/java/com/youknow/data/source/remote/api/model/NowPlayingResp.kt
+++ kotlin-rx/data/src/main/java/com/youknow/data/source/remote/api/model/NowPlayingResp.kt
@@ -4,41 +4,19 @@ import com.google.gson.annotations.SerializedName
 import com.youknow.domain.model.SimpleMovie
 
 data class NowPlayingResp(
-    val page: Int = 0,
-    val results: List<Result> = listOf(),
-    val dates: Dates = Dates(),
-    val totalPages: Int = 0,
-    val totalResults: Int = 0
+    val results: List<Result> = listOf()
 )
 
 data class Result(
     @SerializedName("poster_path")
     val posterPath: String = "",
     val adult: Boolean = false,
-    val overview: String = "",
     @SerializedName("release_date")
     val releaseDate: String = "",
-    @SerializedName("genre_ids")
-    val genreIds: List<Int> = listOf(),
     val id: Int = 0,
-    @SerializedName("original_title")
-    val originalTitle: String = "",
-    @SerializedName("original_language")
-    val originalLanguage: String = "",
     val title: String = "",
-    @SerializedName("backdrop_path")
-    val backdropPath: String = "",
-    val popularity: Float = 0f,
-    @SerializedName("vote_count")
-    val voteCount: Int = 0,
-    val video: Boolean = false,
     @SerializedName("vote_average")
     val voteAverage: Float = 0f
-)
-
-data class Dates(
-    val maximum: String = "",
-    val minimum: String = ""
 )
 
 fun NowPlayingResp.mapToDomain(): List<SimpleMovie> = results.map {
